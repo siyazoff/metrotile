637632a25cd97bb9b473e4ca663f8dcccc8fba91
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-03-07 15:59:19 +0500

    edits from the client

--- js/main.js
+++ js/main.js
@@ -1,52 +1,66 @@
 document.addEventListener("DOMContentLoaded", function () {
   const headerBurger = document.querySelector(".header__burger");
   const popup = document.querySelector(".popup");
+  const header = document.querySelector("#header");
+  const headerBox = document.querySelector(".header__box");
 
-  headerBurger.addEventListener("click", addActiveClass);
+  headerBurger.addEventListener("click", toggleActiveClass);
+  window.addEventListener("scroll", handleScroll);
 
-  function addActiveClass() {
+  function toggleActiveClass() {
     this.classList.toggle("is_active");
     popup.classList.toggle("is_active");
   }
 
   let prevScrollPos = window.scrollY;
-  const header = document.querySelector("#header");
-  const headerBox = document.querySelector(".header__box");
 
-  window.addEventListener("scroll", function () {
+  function handleScroll() {
     const currentScrollPos = window.scrollY;
     const isMobile = window.innerWidth <= 768;
 
-    if (currentScrollPos === 0) {
-      header.style.top = isMobile ? "0px" : "14px";
-      if (!isMobile) {
-        header.style.background = "transparent";
-        headerBox.style.maxWidth = "1290px";
-        headerBox.style.borderRadius = "20px";
-      }
-    } else {
-      header.style.top = prevScrollPos > currentScrollPos ? "0" : "-150px";
-      headerBox.style.maxWidth =
-        prevScrollPos > currentScrollPos ? "100%" : "1290px";
-      if (!isMobile) {
-        header.style.background = "white";
-        headerBox.style.borderRadius =
-          prevScrollPos > currentScrollPos ? "0" : "20px";
-      }
-    }
+    adjustHeaderStyles(currentScrollPos, isMobile);
 
     if (currentScrollPos > prevScrollPos) {
-      headerBurger.classList.remove("is_active");
-      popup.classList.remove("is_active");
+      removeActiveClasses();
     }
 
+    prevScrollPos = currentScrollPos;
+  }
+
+  function adjustHeaderStyles(scrollPos, isMobile) {
+    if (scrollPos === 0) {
+      setInitialStyles(isMobile);
+    } else {
+      setScrolledStyles(isMobile);
+    }
+  }
+
+  function setInitialStyles(isMobile) {
+    header.style.top = isMobile ? "0px" : "14px";
+    if (!isMobile) {
+      header.style.background = "transparent";
+      headerBox.style.maxWidth = "1290px";
+      headerBox.style.borderRadius = "20px";
+    }
+  }
+
+  function setScrolledStyles(isMobile) {
+    if (!isMobile) {
+      header.style.background = "white";
+      headerBox.style.borderRadius = "0";
+      header.style.top = "0px";
+      headerBox.style.maxWidth = "100%";
+    }
+  }
+
+  function removeActiveClasses() {
+    headerBurger.classList.remove("is_active");
+    popup.classList.remove("is_active");
     header.classList.remove("active");
     document
       .querySelectorAll(".burger")
       .forEach((burger) => burger.classList.remove("active"));
-
-    prevScrollPos = currentScrollPos;
-  });
+  }
 
   const faq = document.querySelectorAll(".faq");
 
@@ -87,23 +101,13 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
       var elements = document.querySelectorAll(".background-change");
       elements.forEach(function (element) {
-        console.log(window.scrollY);
         if (window.innerWidth >= 768) {
-          // Проверяем ширину экрана для десктопа
           if (window.scrollY >= 2891) {
             element.style.backgroundAttachment = "unset";
             element.style.backgroundPosition = "46.5% 50%";
           } else {
             element.style.backgroundAttachment = "fixed";
             element.style.backgroundPosition = "47% 50%";
-          }
-        } else {
-          if (window.scrollY >= 1577) {
-            element.style.backgroundAttachment = "unset";
-            element.style.backgroundSize = "100%";
-          } else {
-            element.style.backgroundAttachment = "fixed";
-            element.style.backgroundSize = "90%";
           }
         }
       });
