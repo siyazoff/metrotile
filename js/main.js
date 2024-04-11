5801faae91b3d3d61741ca878d024170cf1e2303
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-04-11 15:10:47 +0500

    animation fly-cart

--- js/main.js
+++ js/main.js
@@ -189,6 +189,63 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  if (window.location.pathname.includes("catalog")) {
+    const shopping_cart = document.querySelector(".widget_cart");
+    const cart_btns = document.querySelectorAll(".catalog-item__add-cart");
+
+    for (cart_btn of cart_btns) {
+      cart_btn.onclick = (e) => {
+        shopping_cart.classList.add("active");
+
+        let product_count =
+          Number(shopping_cart.getAttribute("data-product-count")) || 0;
+        shopping_cart.setAttribute("data-product-count", product_count + 1);
+
+        // finding first grand parent of target button
+        let target_parent = window.matchMedia("(max-width: 768px)").matches
+          ? e.target.parentNode.parentNode.parentNode
+          : e.target.parentNode.parentNode.parentNode.parentNode;
+        // let target_parent =
+        //   e.target.parentNode.parentNode.parentNode.parentNode;
+
+        target_parent.style.zIndex = "100";
+        // Creating separate Image
+        let img = target_parent.querySelector("img");
+        let flying_img = img.cloneNode();
+        flying_img.classList.add("flying-img");
+
+        target_parent.appendChild(flying_img);
+
+        // Finding position of flying image
+
+        const flying_img_pos = flying_img.getBoundingClientRect();
+        const shopping_cart_pos = shopping_cart.getBoundingClientRect();
+
+        let data = {
+          left:
+            shopping_cart_pos.left -
+            (shopping_cart_pos.width / 2 +
+              flying_img_pos.left +
+              flying_img_pos.width / 2),
+          top: shopping_cart_pos.bottom - flying_img_pos.bottom + 65,
+        };
+
+        console.log(data.top);
+
+        flying_img.style.cssText = `
+                                --left : ${data.left.toFixed(2)}px;
+                                --top : ${data.top.toFixed(2)}px;
+                                `;
+
+        setTimeout(() => {
+          target_parent.style.zIndex = "";
+          target_parent.removeChild(flying_img);
+          shopping_cart.classList.remove("active");
+        }, 1000);
+      };
+    }
+  }
+
   var modalForm = new tingle.modal({
     onClose: function () {
       modalForm.setContent("");
@@ -238,4 +295,64 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
   });
+
+  if (window.location.pathname.includes("single")) {
+    const shopping_cart = document.querySelector(".widget_cart");
+    const cart_btns = document.querySelectorAll(
+      ".section-product__add-to-card"
+    );
+
+    for (cart_btn of cart_btns) {
+      cart_btn.onclick = (e) => {
+        shopping_cart.classList.add("active");
+
+        let product_count =
+          Number(shopping_cart.getAttribute("data-product-count")) || 0;
+        shopping_cart.setAttribute("data-product-count", product_count + 1);
+
+        // finding first grand parent of target button
+        let target_parent = window.matchMedia("(max-width: 768px)").matches
+          ? e.target.parentNode.parentNode.parentNode
+          : e.target.parentNode.parentNode.parentNode.parentNode.querySelector(
+              ".section-product__left"
+            );
+        console.log(target_parent);
+
+        target_parent.style.zIndex = "100";
+        // Creating separate Image
+        let img = target_parent.querySelector("img");
+        let flying_img = img.cloneNode();
+        flying_img.classList.add("flying-img-single");
+
+        target_parent.appendChild(flying_img);
+
+        // Finding position of flying image
+
+        const flying_img_pos = flying_img.getBoundingClientRect();
+        const shopping_cart_pos = shopping_cart.getBoundingClientRect();
+
+        let data = {
+          left:
+            shopping_cart_pos.left -
+            (shopping_cart_pos.width / 2 +
+              flying_img_pos.left +
+              flying_img_pos.width / 2),
+          top: shopping_cart_pos.bottom - flying_img_pos.bottom + 240,
+        };
+
+        console.log(data.top);
+
+        flying_img.style.cssText = `
+                                --left : ${data.left.toFixed(2)}px;
+                                --top : ${data.top.toFixed(2)}px;
+                                `;
+
+        setTimeout(() => {
+          target_parent.style.zIndex = "";
+          target_parent.removeChild(flying_img);
+          shopping_cart.classList.remove("active");
+        }, 1000);
+      };
+    }
+  }
 });
